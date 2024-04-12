b77a03aef071ce19ad500e11d0e79ac981c68b01
matthew-b20 <matthewjohnbarrett@gmail.com>
2024-04-11 21:50:37 -0400

    code written out

--- Lab9.py
+++ Lab9.py
@@ -2,7 +2,9 @@
 #Lab 9 Git
 
 def encode(password):
-    for char in password:
+    for int in password:
+        new_number = str(int(digit) + 3)
+        encode_pass += new_number
+    return encoded_pass
 
-h
 print(encode("1234555"))
