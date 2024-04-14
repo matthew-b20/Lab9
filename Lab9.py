8870f242804761760bd4fbd5018f872956156097
kriegle <kriegle@ufl.edu>
2024-04-13 23:32:55 -0400

    added decode, fixed some variable errors

--- Lab9.py
+++ Lab9.py
@@ -3,28 +3,37 @@
 
 def encode(password):
     menu()
-    for int in password:
+    encode_pass = []
+    for digit in password:
         new_number = str(int(digit) + 3)
-        encode_pass += new_number
-    return encoded_pass
+        encode_pass.append(new_number)
+    return ''.join(encode_pass)
 
-print(encode("1234555"))
+def decode(password):
+    menu()
+    decode_pass = []
+    for digit in password:
+        new_number = str(int(digit) - 3)
+        decode_pass.append(new_number)
+    return ''.join(decode_pass)
+
 
 def menu():
-    print("Menu")
-    print("____________")
-    print("1. Encode")
-    print("2. Decode")
-    print("3. Quit")
-    dec = input("Please enter an option:")
-    if dec == "1":
-        password = input("Please enter your password to encode:")
-        encoded = encode(password)
-        print("Your password has been encoded and stored!")
-    elif dec == "2":
-        decoded = decode(encoded)
-        print(f"The encoded password is {encoded}, and the original password is {decoded}")
-        pass
-    elif dec == "3":
-        break
+    while True:
+        print("Menu")
+        print("____________")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        dec = input("Please enter an option:")
+        if dec == "1":
+            password = input("Please enter your password to encode:")
+            encoded = encode(password)
+            print("Your password has been encoded and stored!")
+        elif dec == "2":
+            decoded = decode(encoded)
+            print(f"The encoded password is {encoded}, and the original password is {decoded}")
+            pass
+        elif dec == "3":
+            break
 
